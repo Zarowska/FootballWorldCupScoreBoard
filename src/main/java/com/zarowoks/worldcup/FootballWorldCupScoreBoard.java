2f6952b65bb812d56d7123be5a29af67d0f348c8
oksana <zarowska.ok@gmail.com>
2023-06-08 23:17:01 +0200

    Made it better

--- src/main/java/com/zarowoks/worldcup/FootballWorldCupScoreBoard.java
+++ src/main/java/com/zarowoks/worldcup/FootballWorldCupScoreBoard.java
@@ -9,17 +9,24 @@ public class FootballWorldCupScoreBoard {
 
     public static void main(String[] args) {
 
-        String inputString1 = "a. Mexico - Canada: 0 – 5";
-        String inputString2 = "b. Spain - Brazil: 10 – 2";
-        String inputString3 = "c. Germany - France: 2 – 2";
-        String inputString4 = "d. Uruguay - Italy: 6 – 6";
-        String inputString5 = "e. Argentina - Australia: 3 – 1";
+        List<String> input = List.of(
+                "a. Mexico - Canada: 0 – 5",
+                "b. Spain - Brazil: 10 – 2",
+                "c. Germany - France: 2 – 2",
+                "d. Uruguay - Italy: 6 – 6",
+                "e. Argentina - Australia: 3 – 1"
+        );
 
-        cupBoard.start(parserInputData.parseHomeTeamName(inputString1), parserInputData.parseHomeTeamName(inputString1)).update(parserInputData.parseHomeTeamScore(inputString1), parserInputData.parseAwayTeamScore(inputString1));
-        cupBoard.start(parserInputData.parseHomeTeamName(inputString2), parserInputData.parseHomeTeamName(inputString2)).update(parserInputData.parseHomeTeamScore(inputString2), parserInputData.parseAwayTeamScore(inputString2));
-        cupBoard.start(parserInputData.parseHomeTeamName(inputString3), parserInputData.parseHomeTeamName(inputString3)).update(parserInputData.parseHomeTeamScore(inputString3), parserInputData.parseAwayTeamScore(inputString3));
-        cupBoard.start(parserInputData.parseHomeTeamName(inputString4), parserInputData.parseHomeTeamName(inputString4)).update(parserInputData.parseHomeTeamScore(inputString4), parserInputData.parseAwayTeamScore(inputString4));
-        cupBoard.start(parserInputData.parseHomeTeamName(inputString5), parserInputData.parseHomeTeamName(inputString5)).update(parserInputData.parseHomeTeamScore(inputString5), parserInputData.parseAwayTeamScore(inputString5));
+        input.forEach(str ->
+                cupBoard.start(
+                        parserInputData.parseHomeTeamName(str),
+                        parserInputData.parseHomeTeamName(str)
+                ).update(
+                        parserInputData.parseHomeTeamScore(str),
+                        parserInputData.parseAwayTeamScore(str)
+                )
+        );
+
 
         List<GameBoard> result = cupBoard.getSummary();
 
